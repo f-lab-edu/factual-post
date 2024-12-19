1bfeb3b7f634d623daad7959734535fa80db3d54
jaemoon <nohjaemoon0717@gmail.com>
2024-12-19 20:48:30 +0900

    회원가입 수정

--- User/repository/userRepository.js
+++ User/repository/userRepository.js
@@ -1,27 +1,20 @@
-const db = require('../../databaseConnection');
+const {getConnection, endTransaction} = require('../../databaseConnection');
 
-/**
- * 
- * @param {} User
- * ...
- * 1. beginTransaction() -> commit() -> release()
- * 2. Connection에 있는 함수 사용하기 
- */
 module.exports.save = async (User) => {
     let connection;
     try{
-        connection = await db.getConnection();
+        connection = await getConnection(true);
         const {username, password} = User;
         await connection.query("insert into User (username, password) values (?,?)", [username, password]);
-        await db.commitTransaction(connection);
+        await endTransaction(connection, 'commit');
     } catch(err) {
-        if(connection) await db.rollbackTransaction(connection);
+        if(connection) await endTransaction(connection, 'rollback');
         throw new Error('회원가입에 실패했습니다.');
     }
 }
 
-module.exports.exist = async (username) => {
-    const connection = await db.getConnection();
+module.exports.isExist = async (username) => {
+    const connection = await getConnection();
     try{
         const [result] = await connection.query("select username from User where username = ?", username);
         return result;
--- User/service/userApplicationService.js
+++ User/service/userApplicationService.js
@@ -1,15 +1,16 @@
 const User = require('../Entity/User');
+const LoginUser = require('../Entity/LoginUser');
 const UserService = require('../Service/userService');
 const UserRepository = require('../Repository/userRepository');
 
-exports.signUp = async (username, password) => {
+module.exports.signUp = async (username, password) => {
     try{
         const user = new User(username, password);
-        await UserService.exist(username);
+        await UserService.isExist(username);
         const encodePassword = await UserService.encodePassword(password);
         user.password = encodePassword;
         await UserRepository.save(user);
     } catch(err) {
-        throw new Error(err);
+        throw err;
     }
 }
